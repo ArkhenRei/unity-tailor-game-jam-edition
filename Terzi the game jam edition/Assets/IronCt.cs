41e7fcdd899374d2c1d5566809194c089d3a9331
planetxxxx <akatayakif@gmail.com>
2022-09-20 22:31:24 +0300

    AssetsAdded
    Assets

--- Terzi the game jam edition/Assets/IronCt.cs
+++ Terzi the game jam edition/Assets/IronCt.cs
@@ -6,13 +6,26 @@ public class IronCt : MonoBehaviour
 {
     public bool _dragging;
     public Vector3 defaultPosition;
+    public Vector3 rotationIron;
+    public ParticleSystem steam;
+    public float neededTime = 1000000f;
+    public float keepTime =0f;
+    public Vector2 velocityIron;
+    public GameObject clothes;
+    public GameObject collisionG;
 
     private void Awake()
     {
+        velocityIron = new Vector2(0, 0);
+        rotationIron = new Vector3(15f, 0f, 45f);
         defaultPosition = transform.position;
     }
     void Update()
     {
+        if (_dragging == false)
+        {
+            transform.position = Vector3.Lerp(transform.position, defaultPosition, 3f * Time.deltaTime);
+        }
         if (!_dragging) return;
         var mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = mousePosition;
@@ -21,11 +34,43 @@ public class IronCt : MonoBehaviour
     private void OnMouseDown()
     {
         _dragging = true;
+       
     }
     private void OnMouseUp()
     {
         _dragging = false;
-        transform.position = defaultPosition;
+        
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+    private void OnMouseDrag()
+    {
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, rotationIron, 5f* Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "cloth")
+        {
+            Invoke("playSteam",1f);
+            collisionG = collision.gameObject;
+            
+            
+
+        }
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "cloth")
+        {
+            CancelInvoke();
+        }
+    }
+    public void playSteam()
+    {
+
+        steam.Play();
+        collisionG.SetActive(false);
+        clothes.gameObject.SetActive(true);
+    }
+  
 }
